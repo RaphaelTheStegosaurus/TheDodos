622938469ff3e659ff88d5eadecc7b1cf09744d7
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2026-04-15 10:57:08 -0600

    Adding movement top_down to the player

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -2,31 +2,31 @@ import { Scene, Physics } from "phaser";
 
 export class Player extends Physics.Arcade.Sprite {
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-
+  public zHeight: number = 0;
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, "player-sprite");
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.setCollideWorldBounds(true);
-    this.setBounce(0.2);
+    // this.setCollideWorldBounds(true);
+    // this.setBounce(0.2);
     this.cursors = scene.input.keyboard!.createCursorKeys();
   }
 
   update() {
     const speed = 200;
-
+    this.setVelocity(0);
     if (this.cursors.left.isDown) {
       this.setVelocityX(-speed);
-      this.setFlipX(true);
     } else if (this.cursors.right.isDown) {
       this.setVelocityX(speed);
-      this.setFlipX(false);
-    } else {
-      this.setVelocityX(0);
     }
-
-    if (this.cursors.up.isDown && this.body?.blocked.down) {
-      this.setVelocityY(-350);
+    if (this.cursors.up.isDown) {
+      this.setVelocityY(-speed);
+    } else if (this.cursors.down.isDown) {
+      this.setVelocityY(speed);
+    }
+    if (this.body) {
+      this.body.velocity.normalize().scale(speed);
     }
   }
 }
