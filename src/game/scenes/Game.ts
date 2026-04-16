ab0f9dfd0808a8a57526635716ed83c76e23b93c
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2026-04-15 19:53:59 -0600

    Working on demo tilemap an tileset

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -9,27 +9,27 @@ export class Game extends Scene {
 
   preload() {
     this.load.setPath("assets");
-    this.load.image("tiles", "world-tileset.png");
+    this.load.image("tiles", "demo-ground.jpg");
     this.load.tilemapTiledJSON("map", "large-map.json");
     this.load.image("player-sprite", "character.png");
   }
 
   create() {
-    const mapWidth = 100 * 32;
-    const mapHeight = 100 * 32;
-    const map = this.make.tilemap({ key: "map" });
-
-    const tileset = map.addTilesetImage("TopDownWorld", "tiles");
-
-    const groundLayer = map.createLayer("Ground", tileset!, 0, 0);
-    const wallLayer = map.createLayer("Walls", tileset!, 0, 0);
-
-    wallLayer?.setCollisionByProperty({ collides: true });
-
+    const TILES_X = 100;
+    const TILES_Y = 100;
+    const TILE_SIZE = 32;
+    const map = this.make.tilemap({
+      tileWidth: TILE_SIZE,
+      tileHeight: TILE_SIZE,
+      width: TILES_X,
+      height: TILES_Y,
+    });
+    const tileset = map.addTilesetImage("tileset_demo", "tiles");
+    const layer = map.createBlankLayer("Capa1", tileset!);
+    layer?.randomize(0, 0, TILES_X, TILES_Y, [0]);
     this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
     this.player = new Player(this, 400, 300);
-    this.setupCamera(mapWidth, mapHeight);
+    this.setupCamera(map.widthInPixels, map.heightInPixels);
   }
   update() {
     if (this.player) {
