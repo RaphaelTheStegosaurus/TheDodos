aab093c6e055955815276b140c559889929b01b0
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2026-04-16 22:00:13 -0600

    Creating EffectManager Exporting code from Game.ts

---
+++ src/effects/EffectManager.ts
@@ -0,0 +1,25 @@
+import * as Phaser from "phaser";
+
+export class EffectManager {
+  constructor(private scene: Phaser.Scene) {}
+
+  public createExplosion(x: number, y: number, color: number, count: number) {
+    const particles = this.scene.add.particles(x, y, "tiles", {
+      frame: 10,
+      speed: { min: -100, max: 100 },
+      angle: { min: 0, max: 360 },
+      scale: { start: 1, end: 0 },
+      blendMode: "SCREEN",
+      lifespan: 400,
+      gravityY: 200,
+      tint: color,
+    });
+
+    particles.explode(count);
+    this.scene.time.delayedCall(500, () => particles.destroy());
+  }
+
+  public screenShake(intensity: number = 0.02) {
+    this.scene.cameras.main.shake(intensity, 200);
+  }
+}
